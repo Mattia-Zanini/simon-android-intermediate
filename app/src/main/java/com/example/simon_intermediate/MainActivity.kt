e38cb32a9e2b2179a51f810a21af195c85653b1c
Mattia-Zanini <mattzanini2@gmail.com>
2026-03-30 18:20:46 +0200

    Layout Buttons e Text
    Sistemato il layout dei pulsanti e della text, mancano i pulsanti per spsotarsi alla activity2 e per pulire la text

--- app/src/main/java/com/example/simon_intermediate/MainActivity.kt
+++ app/src/main/java/com/example/simon_intermediate/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.simon_intermediate
 
+import android.R
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,11 +24,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.example.simon_intermediate.ui.theme.SimonIntermediateTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,9 +56,10 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )*/
-                    MainScreen(modifier = Modifier
-                        .fillMaxSize()
-                        .padding(innerPadding)
+                    MainScreen(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
                     )
                 }
             }
@@ -57,62 +68,166 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier)
-{
+fun MainScreen(modifier: Modifier = Modifier) {
     val orientation = LocalConfiguration.current.orientation
 
     // Reference: https://developer.android.com/develop/ui/compose/state-saving
-    var c1 by rememberSaveable { mutableStateOf(false) }
+    var txt by rememberSaveable { mutableStateOf("") }
 
     // Reference: https://developer.android.com/develop/ui/compose/layouts/constraintlayout
     ConstraintLayout(modifier = modifier) {
-        val (sw1, tv, sw2) = createRefs()
+        val (b1, b2, b3, b4, b5, b6, textBox) = createRefs()
 
-        /*
-        Switch(
-            checked = c1,
-            onCheckedChange = { c1 = it },
-            modifier = Modifier.constrainAs(sw1) {
-                start.linkTo(parent.start)
-                top.linkTo(parent.top)
+        // Shape uguale per tutti i pulsanti
+        val btnShape = RoundedCornerShape(6.dp)
+        // Lista contenete tutti i colori dei pulsanti
+        val colors =
+            listOf<Color>(
+                Color.Red,
+                Color.Green,
+                Color.Blue,
+                Color.Magenta,
+                Color.Yellow,
+                Color.Cyan
+            )
+
+        val btnStrings =
+            listOf<String>(
+                "R",
+                "G",
+                "B",
+                "M",
+                "Y",
+                "C"
+            )
+
+        // Margine interno tra i bottoni
+        val innerMargin = 6.dp
+        // Margine esterno tra i bottoni
+        val externalMargin = 16.dp
+
+        // Lista di tutti i modifier dei pulsanti
+        val btnModifiers = listOf<Modifier>(
+            // B1
+            Modifier.constrainAs(b1) {
+                width = Dimension.fillToConstraints
+                height = Dimension.fillToConstraints
+
                 if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                    end.linkTo(tv.start)
-                    bottom.linkTo(parent.bottom)
+                } else {
+                    start.linkTo(parent.start, margin = externalMargin)
+                    end.linkTo(b2.start, margin = innerMargin)
+                    top.linkTo(parent.top, margin = externalMargin)
+                    bottom.linkTo(b3.top, margin = innerMargin)
                 }
-                else {
-                    end.linkTo(parent.end)
-                    bottom.linkTo(tv.top)
+            },
+
+            //B2
+            Modifier.constrainAs(b2) {
+                width = Dimension.fillToConstraints
+                height = Dimension.fillToConstraints
+
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                } else {
+                    start.linkTo(b1.end, margin = innerMargin)
+                    end.linkTo(parent.end, margin = externalMargin)
+                    top.linkTo(parent.top, margin = externalMargin)
+                    bottom.linkTo(b4.top, margin = innerMargin)
+                }
+            },
+
+            //B3
+            Modifier.constrainAs(b3) {
+                width = Dimension.fillToConstraints
+                height = Dimension.fillToConstraints
+
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                } else {
+                    start.linkTo(parent.start, margin = externalMargin)
+                    end.linkTo(b4.start, margin = innerMargin)
+                    top.linkTo(b1.bottom, margin = innerMargin)
+                    bottom.linkTo(b5.top, margin = innerMargin)
+                }
+            },
+
+            //B4
+            Modifier.constrainAs(b4) {
+                width = Dimension.fillToConstraints
+                height = Dimension.fillToConstraints
+
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                } else {
+                    start.linkTo(b3.end, margin = innerMargin)
+                    end.linkTo(parent.end, margin = externalMargin)
+                    top.linkTo(b2.bottom, margin = innerMargin)
+                    bottom.linkTo(b6.top, margin = innerMargin)
+                }
+            },
+
+            //B5
+            Modifier.constrainAs(b5) {
+                width = Dimension.fillToConstraints
+                height = Dimension.fillToConstraints
+
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                } else {
+                    start.linkTo(parent.start, margin = externalMargin)
+                    end.linkTo(b6.start, margin = innerMargin)
+                    top.linkTo(b3.bottom, margin = innerMargin)
+                    bottom.linkTo(textBox.top, margin = externalMargin)
+                }
+            },
+
+            //B6
+            Modifier.constrainAs(b6) {
+                width = Dimension.fillToConstraints
+                height = Dimension.fillToConstraints
+
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                } else {
+                    start.linkTo(b5.end, margin = innerMargin)
+                    end.linkTo(parent.end, margin = externalMargin)
+                    top.linkTo(b4.bottom, margin = innerMargin)
+                    bottom.linkTo(textBox.top, margin = externalMargin)
                 }
             }
         )
-        */
 
-        Button(
-            modifier = Modifier.constrainAs(sw1) {
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-                top.linkTo(parent.top)
-                bottom.linkTo(parent.bottom)
-                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                } else {
+        // Ciclo per disegnare i pulsanti (usando i riferimenti creati sopra)
+        repeat(6) { index ->
+            Button(
+                modifier = btnModifiers[index],
+                shape = btnShape,
+                colors = ButtonDefaults.filledTonalButtonColors(colors[index]),
+                onClick = {
+                    Log.d("MainActivity - Button", "B${index + 1}: ${btnStrings[index]} clicked")
+
+                    txt += btnStrings[index] + ","
                 }
-            },
-            onClick = {},
-            shape = RoundedCornerShape(4.dp)
-        ) {}
+            ) {}
+        }
 
-        /*Text(
-            text = if (orientation == Configuration.ORIENTATION_LANDSCAPE)
-                stringResource(R.string.land)
-            else
-                stringResource(R.string.port),
-            modifier = Modifier.constrainAs(tv) {
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-                top.linkTo(parent.top)
-                bottom.linkTo(parent.bottom)
-            }
-        )*/
+        Text(
+            text = txt,
+            modifier = Modifier
+                .constrainAs(textBox) {
+                    width = Dimension.fillToConstraints
+                    height = Dimension.value(200.dp)
+
+                    start.linkTo(parent.start, margin = 16.dp)
+                    end.linkTo(parent.end, margin = 16.dp)
+                    top.linkTo(b5.bottom)
+                    top.linkTo(b6.bottom)
+                    bottom.linkTo(parent.bottom, margin = 16.dp)
+                }
+                // Applica il colore allo sfondo della text
+                .background(
+                    color = Color.LightGray,
+                    shape = RoundedCornerShape(8.dp)
+                )
+                // Aggiungo il padding (Margine interno), questo sposta il testo lontano dai bordi del box
+                .padding(16.dp),
+        )
     }
 }
 
