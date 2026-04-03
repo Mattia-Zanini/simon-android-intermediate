9bebc5f0f12e16c7fda41508fe4a122146b770f6
Mattia-Zanini <mattzanini2@gmail.com>
2026-04-03 04:18:29 +0200

    Reimplementazione UI
    ho sostituito i constraints utilizzando invece Column e Row per poter gestire meglio e più agilmente il layout senza scrivere una marea di constraints e linkTo sia per il portrait che per il landscape.
    Inoltre ho "scomposto" il layout creando dei composable che rappresentano i singoli elementi dell'interfaccia così da poter riutilizzare gli stessi elementi senza fare un copia incolla eccessivo.

--- app/src/main/java/com/example/simon_intermediate/MainActivity.kt
+++ app/src/main/java/com/example/simon_intermediate/MainActivity.kt
@@ -7,7 +7,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,14 +28,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import com.example.simon_intermediate.ui.theme.SimonIntermediateTheme
 
 class MainActivity : ComponentActivity() {
@@ -63,204 +69,172 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
+    // Recupero l'orientamento attuale del dispositivo
     val orientation = LocalConfiguration.current.orientation
+    val isPortrait: Boolean = orientation == Configuration.ORIENTATION_PORTRAIT
 
-    // Reference: https://developer.android.com/develop/ui/compose/state-saving
+    val tag = "MainActivity"
+
+    // Stato per memorizzare la sequenza di colori cliccati
     var txt by rememberSaveable { mutableStateOf("") }
 
-    // Reference: https://developer.android.com/develop/ui/compose/layouts/constraintlayout
-    ConstraintLayout(modifier = modifier) {
-        val (b1, b2, b3, b4, b5, b6, textBox, endGame, delete) = createRefs()
+    // Lista dei colori per la griglia
+    val colors = listOf(Color.Red, Color.Green, Color.Blue, Color.Magenta, Color.Yellow, Color.Cyan)
+    // Lettere associate ai colori
+    val btnStrings = listOf("R", "G", "B", "M", "Y", "C")
 
-        // Shape uguale per tutti i pulsanti
-        val btnShape = RoundedCornerShape(6.dp)
-        // Lista contenete tutti i colori dei pulsanti
-        val colors =
-            listOf<Color>(
-                Color.Red,
-                Color.Green,
-                Color.Blue,
-                Color.Magenta,
-                Color.Yellow,
-                Color.Cyan
+    // Assegno il numero di colonne e di righe a seconda dell'orientamento
+    val cols: Int = if (isPortrait) 3 else 2
+    val rows: Int = if (isPortrait) 2 else 3
+
+    // Altezza variabile per la TextBox a seconda dell'orientamento
+    val textBoxHeight = if (isPortrait) 180.dp else 200.dp
+
+    // Questa è la lambda che viene utilizzata in entrambi i casi (sia portrait che landscape)
+    val onColorClick: (String) -> Unit = { color ->
+        txt += if (txt == "") color else ", $color"
+        Log.d(tag, "BTN '$color' clicked")
+    }
+
+    if (isPortrait) {
+        // ----- LAYOUT PORTRAIT -----
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp) // Margine esterno
+        ) {
+            // Matrice di pulsanti colorati
+            ColorGrid(Modifier.weight(1f), rows, cols, colors, btnStrings, onColorClick)
+
+            // Text per contenere la sequenza
+            TextBox(
+                Modifier
+                    .fillMaxWidth()
+                    .height(textBoxHeight)
+                    .padding(vertical = 16.dp),
+                txt
             )
 
-        val btnStrings =
-            listOf<String>(
-                "R",
-                "G",
-                "B",
-                "M",
-                "Y",
-                "C"
+            // Zona pulsanti di controllo
+            ActionButtons(
+                onDelete = {
+                    txt = ""
+                    Log.d(tag, "Delete")
+                },
+                onEnd = { Log.d(tag, "End") }
             )
-
-        // Margine interno tra i bottoni
-        val innerMargin = 6.dp
-        // Margine esterno tra i bottoni
-        val externalMargin = 16.dp
-
-        // Lista di tutti i modifier dei pulsanti
-        val btnModifiers = listOf<Modifier>(
-            // B1
-            Modifier.constrainAs(b1) {
-                width = Dimension.fillToConstraints
-                height = Dimension.fillToConstraints
-
-                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                } else {
-                    start.linkTo(parent.start, margin = externalMargin)
-                    end.linkTo(b2.start, margin = innerMargin)
-                    top.linkTo(parent.top, margin = externalMargin)
-                    bottom.linkTo(b3.top, margin = innerMargin)
-                }
-            },
-
-            //B2
-            Modifier.constrainAs(b2) {
-                width = Dimension.fillToConstraints
-                height = Dimension.fillToConstraints
-
-                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                } else {
-                    start.linkTo(b1.end, margin = innerMargin)
-                    end.linkTo(parent.end, margin = externalMargin)
-                    top.linkTo(parent.top, margin = externalMargin)
-                    bottom.linkTo(b4.top, margin = innerMargin)
-                }
-            },
-
-            //B3
-            Modifier.constrainAs(b3) {
-                width = Dimension.fillToConstraints
-                height = Dimension.fillToConstraints
-
-                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                } else {
-                    start.linkTo(parent.start, margin = externalMargin)
-                    end.linkTo(b4.start, margin = innerMargin)
-                    top.linkTo(b1.bottom, margin = innerMargin)
-                    bottom.linkTo(b5.top, margin = innerMargin)
-                }
-            },
-
-            //B4
-            Modifier.constrainAs(b4) {
-                width = Dimension.fillToConstraints
-                height = Dimension.fillToConstraints
-
-                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                } else {
-                    start.linkTo(b3.end, margin = innerMargin)
-                    end.linkTo(parent.end, margin = externalMargin)
-                    top.linkTo(b2.bottom, margin = innerMargin)
-                    bottom.linkTo(b6.top, margin = innerMargin)
-                }
-            },
-
-            //B5
-            Modifier.constrainAs(b5) {
-                width = Dimension.fillToConstraints
-                height = Dimension.fillToConstraints
-
-                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                } else {
-                    start.linkTo(parent.start, margin = externalMargin)
-                    end.linkTo(b6.start, margin = innerMargin)
-                    top.linkTo(b3.bottom, margin = innerMargin)
-                    bottom.linkTo(textBox.top, margin = externalMargin)
-                }
-            },
-
-            //B6
-            Modifier.constrainAs(b6) {
-                width = Dimension.fillToConstraints
-                height = Dimension.fillToConstraints
-
-                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                } else {
-                    start.linkTo(b5.end, margin = innerMargin)
-                    end.linkTo(parent.end, margin = externalMargin)
-                    top.linkTo(b4.bottom, margin = innerMargin)
-                    bottom.linkTo(textBox.top, margin = externalMargin)
-                }
-            }
-        )
-
-        // Ciclo per disegnare i pulsanti (usando i riferimenti creati sopra)
-        repeat(6) { index ->
-            Button(
-                modifier = btnModifiers[index],
-                shape = btnShape,
-                colors = ButtonDefaults.filledTonalButtonColors(colors[index]),
-                onClick = {
-                    Log.d("MainActivity - Button", "B${index + 1}: ${btnStrings[index]} clicked")
-
-                    if (txt == "")
-                        txt += btnStrings[index]
-                    else
-                        txt += ", " + btnStrings[index]
-                }
-            ) {}
         }
+    } else {
+        // ----- LAYOUT LANDSCAPE -----
+        Row(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            // Sinistra: Griglia (55% dello spazio)
+            ColorGrid(Modifier.weight(0.55f), rows, cols, colors, btnStrings, onColorClick)
 
-        // Reference: https://developer.android.com/reference/kotlin/androidx/compose/foundation/rememberScrollState.composable
-        // Crea e "ricorda" un oggetto che mantiene traccia della posizione attuale dello scorrimento.
-        val scrollState = rememberScrollState()
-
-        Text(
-            text = txt,
-            modifier = Modifier
-                .constrainAs(textBox) {
-                    width = Dimension.fillToConstraints
-                    height = Dimension.value(200.dp)
-
-                    start.linkTo(parent.start, margin = 16.dp)
-                    end.linkTo(parent.end, margin = 16.dp)
-
-                    top.linkTo(b5.bottom)
-                    top.linkTo(b6.bottom)
-
-                    bottom.linkTo(delete.bottom, margin = 8.dp)
-                    bottom.linkTo(endGame.bottom, margin = 8.dp)
-                }
-                // Applica il colore allo sfondo della text
-                .background(
-                    color = Color.LightGray,
-                    shape = RoundedCornerShape(8.dp)
+            // Destra: TextBox + Pulsanti (45% dello spazio)
+            Column(
+                modifier = Modifier
+                    .weight(0.45f)
+                    .fillMaxHeight()
+            ) {
+                // Text per contenere la sequenza
+                TextBox(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(textBoxHeight),
+                    txt
                 )
-                // Aggiungo il padding (Margine interno), questo sposta il testo lontano dai bordi del box
-                .padding(16.dp)
 
-                // Collega lo stato al modificatore per abilitare lo scroll
-                .verticalScroll(scrollState),
-        )
+                // Questo Spacer "mangia" tutto lo spazio che avanza tra la TextBox e l'ActionButtons
+                Spacer(modifier = Modifier.weight(1f))
 
-        // Cancella
-        Button(
-            modifier = Modifier.constrainAs(delete) {
-                start.linkTo(parent.start, margin = 16.dp)
-                top.linkTo(textBox.bottom)
-                bottom.linkTo(parent.bottom, margin = 8.dp)
-            },
-            onClick = {
-                Log.d("MainActivity - Button", "'Delete' clicked")
-
-                txt = ""
+                // Zona pulsanti di controllo
+                ActionButtons(
+                    onDelete = {
+                        txt = ""
+                        Log.d(tag, "Delete")
+                    },
+                    onEnd = { Log.d(tag, "End") }
+                )
             }
-        ) { Text(stringResource(R.string.delete_sequence_btn)) }
+        }
+    }
+}
 
-        // Fine Partita
-        Button(
-            modifier = Modifier.constrainAs(endGame) {
-                end.linkTo(parent.end, margin = 16.dp)
-                top.linkTo(textBox.bottom)
-                bottom.linkTo(parent.bottom, margin = 8.dp)
-            },
-            onClick = {
-                Log.d("MainActivity - Button", "'End Game' clicked")
+@Composable
+fun ColorGrid(
+    modifier: Modifier = Modifier,
+    rows: Int,
+    cols: Int,
+    colors: List<Color>,
+    colorsStrings: List<String>,
+    onButtonClick: (String) -> Unit
+) {
+    Column(modifier = modifier) {
+        var index = 0
+
+        repeat(cols) {
+            Row(
+                modifier = Modifier
+                    // Distribuisco equamente lo spazio verticale tra le righe
+                    .weight(1f)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp) // Spazio tra colonne
+            ) {
+                repeat(rows) {
+                    val i = index
+
+                    Button(
+                        modifier = Modifier
+                            // Distribuisco equamente lo spazio orizzontale tra i pulsanti
+                            .weight(1f)
+                            .fillMaxHeight()
+                            .padding(vertical = 4.dp), // Spazio tra le righe
+                        shape = RoundedCornerShape(6.dp),
+                        colors = ButtonDefaults.filledTonalButtonColors(colors[i]),
+                        onClick = { onButtonClick(colorsStrings[i]) }
+                    ) { }
+
+                    // Incremento l'indice per scorrere le liste dei colori e delle stringhe
+                    index++
+                }
             }
-        ) { Text(stringResource(R.string.end_game_btn)) }
+        }
+    }
+}
+
+
+@Composable
+fun TextBox(modifier: Modifier = Modifier, txt: String) {
+    // Reference: https://developer.android.com/reference/kotlin/androidx/compose/foundation/rememberScrollState.composable
+    // Crea e "ricorda" un oggetto che mantiene traccia della posizione attuale dello scorrimento.
+    val scrollState = rememberScrollState()
+
+    Text(
+        text = txt,
+        modifier = modifier
+            // Sfondo grigio con angoli arrotondati
+            .background(Color.LightGray, RoundedCornerShape(8.dp))
+            // Padding interno al testo (è interno perchè applico il padding DOPO il background)
+            .padding(16.dp)
+            // Abilito lo scorrimento verticale se il testo eccede lo spazio
+            .verticalScroll(scrollState)
+    )
+}
+
+@Composable
+fun ActionButtons(onDelete: () -> Unit, onEnd: () -> Unit) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        // Allineo i pulsanti ai lati opposti della riga lasciando, per l'appunto, uno spazio in mezzo tra di loro
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Button(onClick = onDelete) { Text(stringResource(R.string.delete_btn)) }
+        Button(onClick = onEnd) { Text(stringResource(R.string.end_game_btn)) }
     }
 }
 
